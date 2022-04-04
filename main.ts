9d4d19063f06b83455aa57a163e1430de92ffde2
Juan9899 <98743364+Juan9899@users.noreply.github.com>
2022-04-05 00:08:40 +0200

    Actualizar main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -3,15 +3,27 @@ radio.onReceivedNumber(function (receivedNumber) {
         basic.showIcon(IconNames.No)
     }
 })
+input.onLogoEvent(TouchButtonEvent.Pressed, function () {
+    if (_1 == 1) {
+        radio.sendNumber(8)
+        _1 += 0
+    }
+    if (_1 == 0) {
+        radio.sendNumber(7)
+        _1 += 1
+    }
+})
 input.onButtonPressed(Button.A, function () {
     radio.sendNumber(1)
 })
 input.onButtonPressed(Button.B, function () {
     radio.sendNumber(2)
 })
+let _1 = 0
 radio.setGroup(86)
 music.playMelody("E B C5 A B G A F ", 120)
 basic.showString("hola")
+_1 = 1
 basic.forever(function () {
     if (input.rotation(Rotation.Roll) > 50) {
         radio.sendNumber(3)
